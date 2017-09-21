aad90f2eba32bb3c64c3a86aae12e2aeb2706524
INSU PARK <insu.park@outlook.com>
2017-09-20 20:17:35 -0700

    Adding embossing func

--- edge_detection.py
+++ edge_detection.py
@@ -74,13 +74,36 @@ def sharpening(img, rows, cols):
     cv2.imshow('Edge Enhancement', ouput_3)
 
     cv2.waitKey(0)
+def embossing(img, rows, cols):
+    # generating the kernels
+    kernel_emboss_1 = np.array([[0, -1, -1],
+                                [1, 0, -1],
+                                [1, 1, 0]])
+    kernel_emboss_2 = np.array([[-1, -1, 0],
+                                [-1, 0, 1],
+                                [0, 1, 1]])
+    kernel_emboss_3 = np.array([[1, 0, 0],
+                                [0, 0, 0],
+                                [0, 0, -1]])
+    # converting the image to grayscale
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # applying the kernels to the grayscale image and adding the offset
+    output_1 = cv2.filter2D(gray_img, -1, kernel_emboss_1) + 128
+    output_2 = cv2.filter2D(gray_img, -1, kernel_emboss_2) + 128
+    output_3 = cv2.filter2D(gray_img, -1, kernel_emboss_3) + 128
+
+    cv2.imshow('Embossing - South West', output_1)
+    cv2.imshow('Embossing - South East', output_2)
+    cv2.imshow('Embossing - North West', output_3)
 #img = cv2.imread('./Images/samsung_sd.jpg', cv2.IMREAD_GRAYSCALE)
 #rows, cols = img.shape
 #edge_detector(img, rows, cols)
 #blur(img, rows, cols)
 
-img = cv2.imread('./Images/samsung_sd.jpg')
+img = cv2.imread('./Images/house.jpg')
 rows, cols = img.shape[:2]
 cv2.imshow('Original', img)
-sharpening(img, rows, cols)
+#sharpening(img, rows, cols)
 #motion_blur(img, rows, cols)
+#embossing(img, rows, cols)
