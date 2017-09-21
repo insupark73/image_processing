c4c1208eaa9cdc3a2ce27ce36a1c1a1a0e1a9e88
INSU PARK <insu.park@outlook.com>
2017-09-20 23:54:26 -0700

    Add new image and functions

--- edge_detection.py
+++ edge_detection.py
@@ -84,26 +84,69 @@ def embossing(img, rows, cols):
                                 [0, 1, 1]])
     kernel_emboss_3 = np.array([[1, 0, 0],
                                 [0, 0, 0],
-                                [0, 0, -1]])
-    # converting the image to grayscale
-    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    # applying the kernels to the grayscale image and adding the offset
-    output_1 = cv2.filter2D(gray_img, -1, kernel_emboss_1) + 128
-    output_2 = cv2.filter2D(gray_img, -1, kernel_emboss_2) + 128
-    output_3 = cv2.filter2D(gray_img, -1, kernel_emboss_3) + 128
-
-    cv2.imshow('Embossing - South West', output_1)
+                                [0, 0, -1]]) # converting the image to grayscale gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # applying the kernels to the grayscale image and adding the offset output_1 = cv2.filter2D(gray_img, -1, kernel_emboss_1) + 128 output_2 = cv2.filter2D(gray_img, -1, kernel_emboss_2) + 128 output_3 = cv2.filter2D(gray_img, -1, kernel_emboss_3) + 128 cv2.imshow('Embossing - South West', output_1)
     cv2.imshow('Embossing - South East', output_2)
     cv2.imshow('Embossing - North West', output_3)
+
+    cv2.waitKey(0)
+
+def morphology(img, rows, cols):
+    kernel = np.ones((2,2), np.uint8)
+
+    img_erosion = cv2.erode(img, kernel, iterations=1)
+    img_dilation = cv2.dilate(img, kernel, iterations=1)
+
+    cv2.imshow('Erosion', img_erosion)
+    cv2.imshow('Dilation', img_dilation)
+
+    cv2.waitKey(0)
+
+def vignette(img, rows, cols):
+    # generating vignette mask using Gaussian kernels
+    kernel_x = cv2.getGaussianKernel(int(1.5*cols), 200)
+    kernel_y = cv2.getGaussianKernel(int(1.5*rows), 200)
+
+    kernel = kernel_y * kernel_x.T 
+    mask = 255 * kernel / np.linalg.norm(kernel)
+    mask = mask[int(0.5*rows):, int(0.5*cols):]
+    output = np.copy(img)
+
+    # applying the mask to each channel in the input image
+    for i in range(3):
+        output[:, :, i] = output[:, :, i] * mask
+
+    cv2.imshow('Vignette', output)
+    cv2.waitKey(0)
+
+def histo_equal(img, rows, cols):
+    # equalize the histogram of the input image
+    histeq = cv2.equalizeHist(img)
+
+    cv2.imshow('Histogram equalized', histeq)
+    cv2.waitKey(0)
+
+def histo_equal_color(img, rows, cols):
+    img_yuv = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
+
+    # equalize the histogram of the Y channel
+    img_yuv[:,:,0] = cv2.equalizeHist(img_yuv[:,:,0])
+
+    # convert the YUV image back to RGB format
+    img_output = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR)
+
+    cv2.imshow('Histogram equalized', img_output)
+    cv2.waitKey(0)
 #img = cv2.imread('./Images/samsung_sd.jpg', cv2.IMREAD_GRAYSCALE)
 #rows, cols = img.shape
 #edge_detector(img, rows, cols)
 #blur(img, rows, cols)
 
-img = cv2.imread('./Images/house.jpg')
+img = cv2.imread('./Images/snow.jpg', 0)
 rows, cols = img.shape[:2]
 cv2.imshow('Original', img)
 #sharpening(img, rows, cols)
 #motion_blur(img, rows, cols)
 #embossing(img, rows, cols)
+#morphology(img, rows, cols)
+#vignette(img, rows, cols)
+histo_equal_color(img, rows, cols)
